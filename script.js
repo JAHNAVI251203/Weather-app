69fe478844fed96f56ff54d3e7af92ed771065c0
JAHNAVI S <jahnavi.satish2003@gmail.com>
2026-01-16 17:23:49 +0530

    fixed broken script in html

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // test change
-
+      console.log("JS file loaded")
       const apiKey = "c3170d230fc6d18e6c84076948add182";
       const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
